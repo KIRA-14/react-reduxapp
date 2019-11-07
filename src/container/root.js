8e37eaa248cc5efadbb895919a368bda22ed1772
Ravikiran <39284822+RavikiranRaman@users.noreply.github.com>
2019-11-07 09:30:33 +0530

    new changes

--- src/container/root.js
+++ src/container/root.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../store/store'
-import AsyncApp from './AsyncApp'
+import AsyncApp from '../components/App'
 
 const store = configureStore()
 
